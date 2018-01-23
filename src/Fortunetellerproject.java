3f53839c4a9ae050089d231600e6dd43c10df06f
Rochelle Taylor <rochelletaylor614@gmail.com>
2018-01-23 09:30:02 -0500

    final commit

--- src/Fortunetellerproject.java
+++ src/Fortunetellerproject.java
@@ -10,7 +10,7 @@ public class Fortunetellerproject {
 		String enterName = input.nextLine();
 
 		System.out.println("Enter last name");
-		String enterLastName = input.nextLine();
+		String lastName = input.nextLine();
 
 		System.out.println("Whats your age?");
 		String enterAge = input.nextLine();
@@ -18,64 +18,89 @@ public class Fortunetellerproject {
 		System.out.println("What is your birth month?");
 		String enterMonth = input.nextLine();
 
-		System.out.println("Whats your favorite ROYGBIV color?");
-		String enterColor = input.nextLine();
+		System.out.println("Whats your favorite ROYGBIV color? If you dont know what ROYGBIV is type help");{
+		String enterROYGBIV = input.nextLine();
+		if(enterROYGBIV.equals("help")){
+			System.out.println("Enter Red Orange Yellow Green Blue Indigo Violet");
+			enterROYGBIV=input.next();
 
-		System.out.println("Will retire in");
+		}
 
-		int yearsRetire = input.nextInt();
+		System.out.println("Enter your age");
 
-		if (yearsRetire % 2 == 0) {
-			System.out.println(yearsRetire + " is even");
-			String yearsRetire1 = input.nextLine();
-			System.out.println("You will retire in 20 years");
+		int age = input.nextInt();
+
+		if (age % 2 == 0) {
+			System.out.println( age + " is even");
+			System.out.println(" You will retire in 20 years");
 
 		} else {
-			System.out.println(yearsRetire + " is odd");
-			String yearsRetire1 = input.nextLine();
+			System.out.println( age + " is odd");
 			System.out.println("You will retire in 25 years");
 
 		}
 
 		System.out.println("How many siblings do you have");
-		String siblings = input.nextLine();
-
-		int siblings1 = 1;
+		int numberSiblings= input.nextInt();
 		String vacation;
-		if (siblings1 == 1) {
+		if (numberSiblings == 1) {
 			vacation = "columbus,Oh";
-		} else if (siblings1 == 2) {
+			
+		} else if (numberSiblings == 2) {
 			vacation = "Lake Michigan";
-		} else if (siblings1 == 3) {
+			
+		} else if (numberSiblings == 3) {
 			vacation = "Toronto";
-		} else if (siblings1 == 4) {
+			
+		} else if (numberSiblings == 4) {
 			vacation = "California";
-		} else if (siblings1 >= 5) {
-			vacation = "Baton Rouge";
+			
+		} else if (numberSiblings >= 5) {
+			vacation = "Texas";
+			
 		} else {
-			vacation = "Chernobyl, Ukraine";
-		}
-		System.out.println(vacation);
-
-		System.out.println("Whats your favorite color?");
-		String color = input.nextLine();
-
-		String transportation;
-		if (color.equals("red")) {
-			transportation = "Chevy";
-		} else if (color.equals("yellow")) {
-			transportation = "Ford";			
-		} else if (color.equals("purple")) {
-			transportation = "Honda";
-		} else if (color.equals("green")) {
-			transportation = "Dodge";
-		} else if (color.equals("brown")) {
-			transportation = "Mustang";
-		} else if (color.equals("orange")) {			
-			
-			
+			vacation = "Pennsylvania";
 		}
 		
+		
+		String transportation=null;
+		if (enterROYGBIV.equals("red")) {
+			transportation = "Chevy";
+		} else if (enterROYGBIV.equals("orange")) {
+			transportation = "Ford";			
+		} else if (enterROYGBIV.equals("yellow")) {
+			transportation = "Honda";
+		} else if (enterROYGBIV.equals("green")) {
+			transportation = "Dodge";
+		} else if (enterROYGBIV.equals("blue")) {
+			transportation = "Mustang";
+		} else if (enterROYGBIV.equals("indigo")) {
+			transportation= "Lincoln";	
+		} else if (enterROYGBIV.equals("violet")) {
+			transportation= "Toyota";
+		
+		}
+		System.out.println("What month were you born?");
+		int birthMonth = input.nextInt();
+		String balance=null;
+		
+		if(birthMonth >= 1 && birthMonth <= 4) {
+			balance = "300,000.00";
+		} else if(birthMonth >= 5 && birthMonth <= 8) {
+			balance = "$400,000.00";
+		}else if(birthMonth >= 9 && birthMonth <= 12) {
+			balance = "$500,000.00";
+		}
+		
+		input.close();
+		
+		System.out.println(enterName + " " + lastName + " will retire in" + age + " years with " 
+		+ balance + " in the bank, a vaction home in " + vacation + ",and travel by" + transportation + ".");
+		}
+	}
+}
 
-	} // End of Main()
-} // End of class Fortuneteller
+
+
+
+
